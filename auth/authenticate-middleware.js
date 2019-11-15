3226a64ebc4e6798e1090d55183e748c94a1b550
jambis <imjaamees@gmail.com>
2019-11-15 11:30:54 -0600

    MVP with 6 tests

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,22 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  const secret = process.env.JWT_SECRET || "a3289rh3298fdn2983hj2nf90-9-n@!)@0";
+
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ message: "Invalid Credentials" });
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "Invalid Credentials" });
+  }
 };
